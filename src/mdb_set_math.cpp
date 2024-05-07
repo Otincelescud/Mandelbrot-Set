4a136116c0400a55c7215d9c41fc9bb494c2eb6d
Otincelescu Darius Marian <otincelescud@gmail.com>
2024-05-07 14:23:28 +0300

    Write cnum_to_px_pos Method

--- src/mdb_set_math.cpp
+++ src/mdb_set_math.cpp
@@ -4,3 +4,8 @@ void MdbSetMath::px_pos_to_cnum(int *px_x, int *px_y, Complex *pan, double long 
     cnum->setReal(pan->getReal() + *px_x * *zoom);
     cnum->setImaginary(pan->getImaginary() - *px_y * *zoom);
 }
+
+void MdbSetMath::cnum_to_px_pos(Complex *cnum, Complex *pan, double long *zoom, int *px_x, int *px_y) {
+    *px_x = int((cnum->getReal() - pan->getReal()) / *zoom);
+    *px_y = int((cnum->getImaginary() - pan->getImaginary()) / *zoom);
+}
